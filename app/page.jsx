031cce89bd9e624316f14b27176bd40860a03524
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-21 16:11:43 +0300

    ..

--- app/page.jsx
+++ app/page.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         </span>
       </h1>
       <p className="desc text-center">
-        Promptopia is anaopen-source AI prompting tool for modern world to discover, create and share creative prompts
+        Promptopia is an aopen-source AI prompting tool for modern world to discover, create and share creative prompts
       </p>
 
       <Feed />
---
+++ app/profile/page.jsx
@@ -0,0 +1,40 @@
+"use client"
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
+
+import Profile from "@components/Profile"
+
+const MyProfile = () => {
+  const { data: session } = useSession()
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`)
+      const data = await response.json()
+      setPosts(data)
+    }
+    if (session?.user.id) getPosts()
+  }, [])
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+
+  return (
+    <Profile
+      name={session.name}
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile
