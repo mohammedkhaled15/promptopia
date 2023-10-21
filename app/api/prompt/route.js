14346d473ad3e5e72879deeb4e18fdb97cf3bb1d
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-21 14:59:24 +0300

    ..

---
+++ app/api/prompt/route.js
@@ -0,0 +1,13 @@
+import { connectDb } from "@utils/connectDb"
+import Prompt from "@models/prompt"
+
+export const GET = async(req)=>{
+  try {
+    await connectDb()
+    const posts = await Prompt.find().populate("creator")
+    return new Response(JSON.stringify(posts),{status:200})
+  } catch (error) {
+    console.log(error)
+    return new Response("Failed to fetch prompts",{status:500})
+  }
+}
