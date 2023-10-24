b421e8cb33e5d0981647ac33c54748d5507060d8
mohammedkhaled15 <mohammedkhaled1590@gmail.com>
2023-10-24 22:35:02 +0300

    finalize

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Profile from "@components/Profile"
+import { useSession } from "next-auth/react"
 import { useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 
@@ -8,6 +9,9 @@ import { useEffect, useState } from "react"
 const UserProfilePage = async ({ params }) => {
   const searchParams = useSearchParams()
   const username = searchParams.get("name")
+
+  const { data: session } = useSession()
+  console.log(session)
 
   const [posts, setPosts] = useState([])
 
@@ -17,12 +21,12 @@ const UserProfilePage = async ({ params }) => {
       const data = await response.json()
       setPosts(data)
     }
-    if (params.id) getPosts()
-  }, [params.id, posts])
+    if (params?.id) getPosts()
+  }, [params.id])
 
   return (
     <Profile
-      name={username}
+      name={username === session?.user?.name.toLocaleLowerCase().replaceAll(" ", "") ? "My" : username}
       desc="Welcome to your personalized profile page"
       data={posts}
     />
